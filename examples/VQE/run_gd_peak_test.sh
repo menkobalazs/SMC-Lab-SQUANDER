87131cc196542689c9f0785b5ae6b088784f5789
Menkó Balázs <menkobalazs@gmail.com>
2025-03-26 19:13:19 +0100

    upload presentations and report in 'docs' folder; update README.md

--- examples/VQE/run_gd_peak_test.sh
+++ examples/VQE/run_gd_peak_test.sh
@@ -1,10 +1,13 @@
 ##### //MB\\
 
-LAYERS=60
-QUBITS=12
+LAYERS=100
+QUBITS=10
 
 # Opt: GRAD_DESCEND
-hwloc-bind --membind node:0 --cpubind node:0 -- python Heisenberg_VQE.py --optimizer=grad_descend --init_parameters=random --layers=$LAYERS --qbit_num=$QUBITS
+#hwloc-bind --membind node:0 --cpubind node:0 -- python Heisenberg_VQE.py --optimizer=grad_descend --init_parameters=random --layers=$LAYERS --qbit_num=$QUBITS
+
+hwloc-bind --membind node:0 --cpubind node:0 -- python Heisenberg_VQE.py --optimizer=cosine --init_parameters=random --layers=$LAYERS --qbit_num=$QUBITS
+
 
 ##### \\MB//
 
